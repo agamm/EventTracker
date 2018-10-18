14cb8367e766f643cfe43abddef7c1ab54015b3b
agam360 <agam360@gmail.com>
2018-10-19 00:06:10 +0300

    Add basic jdocs for methods

--- eventtracker/src/main/java/com/sdk/agam/eventtracker/EventMessage.java
+++ eventtracker/src/main/java/com/sdk/agam/eventtracker/EventMessage.java
@@ -7,16 +7,29 @@ public class EventMessage {
     private String actionKey;
     private JSONObject data;
 
+    /**
+     * The constructor for a new EventMessage
+     * Used for clarification for events structure.
+     * @param actionKey the identifier of the current event (network, location ...)
+     * @param data specific information for the current event.
+     */
     EventMessage(String actionKey, JSONObject data) {
         this.actionKey = actionKey;
         this.data = data;
     }
 
+    /**
+     * Basic getter for actionKey
+     * @return String actionKey
+     */
     public String getActionKey() {
         return actionKey;
     }
 
-
+    /**
+     * Basic getter for data
+     * @return JSONObject data
+     */
     public JSONObject getData() {
         return data;
     }
